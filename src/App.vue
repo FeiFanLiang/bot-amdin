0f7cff4d1cafb9844035ef3d79ba1a3a07422a67
alex lee <w656957526@vip.qq.com>
2021-08-04 17:48:26 +0800

    feat: first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+<style lang="scss">
+.table-box {
+  padding: 10px;
+}
+.pagintion {
+  text-align: center;
+}
+</style>
---
+++ src/api/user.js
@@ -0,0 +1,32 @@
+import request from '@/utils/request'
+
+export function login(data) {
+  return request({
+    url: '/api/login',
+    method: 'post',
+    data
+  })
+}
+
+export const updatePasswordApi = (data) => {
+  return request({
+    url:"/api/updatePassword",
+    method:"post",
+    data
+  })
+}
+
+export function getInfo(token) {
+  return request({
+    url: '/vue-element-admin/user/info',
+    method: 'get',
+    params: { token }
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/api/logout',
+    method: 'get'
+  })
+}
