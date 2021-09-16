c23a6231e84865eb5cd20cc9c4a6369827505e06
alex lee <w656957526@vip.com>
2021-09-16 20:22:09 +0800

    f

--- src/views/enery-list/index.vue
+++ src/views/enery-list/index.vue
@@ -2,24 +2,24 @@
   <div class="app-container">
     <el-form ref="form" :rules="rules" :model="form" label-width="200px">
       <el-form-item label="USDT到ETH比率" prop="ethRate">
-          <el-input-number :precision="2" v-model="form.ethRate" placeholder="输入"></el-input-number>
+        <el-input-number :precision="6" v-model="form.ethRate" placeholder="输入"></el-input-number>
       </el-form-item>
       <el-form-item label="USDT到TRX比率" prop="trxRate">
-          <el-input-number :precision="2" v-model="form.trxRate" placeholder="输入"></el-input-number>
+        <el-input-number :precision="2" v-model="form.trxRate" placeholder="输入"></el-input-number>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="onSubmit">保存</el-button>
       </el-form-item>
     </el-form>
-     <el-form ref="handingForm" :rules="handingRules" :model="handingForm" label-width="200px">
+    <el-form ref="handingForm" :rules="handingRules" :model="handingForm" label-width="200px">
       <el-form-item label="trc手续费" prop="trc">
-          <el-input-number :precision="0" v-model="handingForm.trc" placeholder="输入"></el-input-number>
+        <el-input-number :precision="0" v-model="handingForm.trc" placeholder="输入"></el-input-number>
       </el-form-item>
       <el-form-item label="erc手续费(固定金额)" prop="erc">
-          <el-input-number :precision="0" v-model="handingForm.erc" placeholder="输入"></el-input-number>
+        <el-input-number :precision="0" v-model="handingForm.erc" placeholder="输入"></el-input-number>
       </el-form-item>
       <el-form-item label="erc手续费(按比率)" prop="ercRate">
-          <el-input-number :precision="3" v-model="handingForm.ercRate" placeholder="输入"></el-input-number>
+        <el-input-number :precision="3" v-model="handingForm.ercRate" placeholder="输入"></el-input-number>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="handingSubmit">保存</el-button>
@@ -29,84 +29,85 @@
 </template>
 
 <script>
-import {getEnergyApi,updateEnergyApi,getHandingFeeApi,updateHandingFeeApi} from '@/api/account'
+import { getEnergyApi, updateEnergyApi, getHandingFeeApi, updateHandingFeeApi } from '@/api/account'
 export default {
   data() {
     return {
       form: {
-        ethRate:'',
-        trxRate:''
+        ethRate: '',
+        trxRate: ''
       },
-      handingForm:{
-        trc:'',
-        erc:'',
-        ercRate:''
+      handingForm: {
+        trc: '',
+        erc: '',
+        ercRate: ''
       },
-      rules:{
-          ethRate:[
-              {
-                  required:true,
-                  message:'必填'
-              },
-              {
-                  type:"number",
-                  min:0,
-                  message:'最小是0'
-              }
-          ],
-          trxRate:[
-              {
-                  required:true,
-                  message:'必填'
-              },
-              {
-                  type:"number",
-                  min:0,
-                  message:'最小是0'
-              }
-          ]
-      },
-      handingRules:{
-        trc:[{
-          required:true,
-          message:'必填'
-        }],
-        erc:[
+      rules: {
+        ethRate: [
           {
-          required:true,
-          message:'必填'
-        }
+            required: true,
+            message: '必填'
+          },
+          {
+            type: "number",
+            min: 0,
+            message: '最小是0'
+          }
         ],
-        ercRate:[
-          {required:true,
-          message:'必填'
+        trxRate: [
+          {
+            required: true,
+            message: '必填'
+          },
+          {
+            type: "number",
+            min: 0,
+            message: '最小是0'
+          }
+        ]
+      },
+      handingRules: {
+        trc: [{
+          required: true,
+          message: '必填'
+        }],
+        erc: [
+          {
+            required: true,
+            message: '必填'
+          }
+        ],
+        ercRate: [
+          {
+            required: true,
+            message: '必填'
 
           }
         ]
       }
     }
   },
-  created(){
-      getEnergyApi().then((res) => {
-          if(res){
-              this.form = {...res}
-          }
-      })
-      getHandingFeeApi().then((res) => {
-        if(res){
-          this.handingForm = {...res}
-        }
-      })
+  created() {
+    getEnergyApi().then((res) => {
+      if (res) {
+        this.form = { ...res }
+      }
+    })
+    getHandingFeeApi().then((res) => {
+      if (res) {
+        this.handingForm = { ...res }
+      }
+    })
   },
   methods: {
     onSubmit() {
       this.$refs['form'].validate().then(() => {
         updateEnergyApi(this.form).then((res) => {
           this.$message.success('更新成功')
-      })
+        })
       })
     },
-    handingSubmit(){
+    handingSubmit() {
       this.$refs['handingForm'].validate().then(() => {
         updateHandingFeeApi(this.handingForm).then(() => {
           this.$message.success('更新成功')
@@ -118,7 +119,7 @@ export default {
 </script>
 
 <style scoped>
-.line{
+.line {
   text-align: center;
 }
 </style>
